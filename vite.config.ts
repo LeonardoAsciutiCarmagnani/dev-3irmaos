66e360ccedc55d2af2ecd7465435a7904cd5cc57
Leonardo Asciuti Carmagnani <lasciuti.c@gmail.com>
2024-11-26 11:59:47 -0300

    Feat - Context zustand

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
       input: "./index.html",
       output: {
         manualChunks: {
-          vendor: ["react", "react-dom"], // Separe bibliotecas comuns em um chunk
+          vendor: ["react", "react-dom"],
         },
       },
     },
