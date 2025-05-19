5da7499eb2ee113e09afb44d97eb4d4cf53fceb3
Lucas Rodrigues Seidel Silva <lucas.rodrigues28012003@gmail.com>
2025-05-19 15:31:06 -0300

    Ajuste na geração de PDF

--- functions/src/controllers/Order/OrderController.ts
+++ functions/src/controllers/Order/OrderController.ts
@@ -632,6 +632,8 @@ export class OrderController {
 
       const browser = await puppeteer.launch({
         headless: true, // recomendação atual
+        args: ["--no-sandbox"],
+        executablePath: puppeteer.executablePath(), // <- ESSENCIAL no Firebase
       });
 
       const page = await browser.newPage();
